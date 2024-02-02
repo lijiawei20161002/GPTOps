4061878b7cd7c9583a492a6173c565d6a1be5a21
李佳蔚 <lijiawei@lijiaweis-Mac-mini.local>
2024-02-02 16:18:10 +0800

    add azure time series data

--- tasks/dynamic vm pricing/scripts/generate_price_data.py
+++ tasks/dynamic vm pricing/scripts/generate_price_data.py
@@ -2,8 +2,11 @@ import utils
 import json
 import pandas as pd
 
-rawdata = utils.read_aws_prices("aws/cost/us-east-1", summary=False)
+#rawdata = utils.read_aws_prices("aws/cost/us-east-1", summary=False)
+rawdata = utils.read_azure_prices("azure/cost")
 for key in rawdata.keys():
     df = pd.DataFrame(rawdata[key])
-    for zone in df['AvailabilityZone'].unique():
-        df[df['AvailabilityZone']==zone][['Timestamp', 'SpotPrice']].to_csv(key+'_'+zone+'.txt', index=False)
+    for zone in df[0].unique():
+        price = df[df[0]==zone][[1, 2]]
+        price = price.rename(columns={1: "Timestamp", 2: "SpotPrice"})
+        price.to_csv(key+'_'+zone+'.txt', index=False)
