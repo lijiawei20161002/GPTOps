c3d27e2b037a0e181fbb1e620af072c9a44571eb
李佳蔚 <lijiawei@lijiaweis-Mac-mini.local>
2024-02-26 03:52:57 +0800

    update

--- agents/run_conversation.py
+++ agents/run_conversation.py
@@ -9,8 +9,8 @@ from summary_agent import SummaryAgent
 from prediction_agent import PredictionAgent
 import pandas as pd
 
-your_api_key = ''
-your_org = ''
+your_api_key = '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'
+your_org = 'org-8yxc3dK5pYpaart4dR32FOda'
 token_limit = 100
 latest_period = 10
 fulldata = pd.read_csv('test_data.txt')
@@ -21,7 +21,15 @@ for i in range(0, len(fulldata), latest_period):
     rawdata=fulldata[start:end]
     summary = summary_agent.run(rawdata=rawdata, start=start, end=end, token_limit=token_limit, latest_period=latest_period)
     print(summary)
-
-#prediction_agent = PredictionAgent(openai_api_key=your_api_key, openai_organization=your_org)
-#prediction_agent.run(summary=summary)
+    example='When input summary on from 0 to 10 days is: \
+    <current> The latest data shows that the spot price for our infrastructure has been fluctuating between 0.3917 and 0.4019. There is a slight decrease in the spot price compared to the previous days. \
+    <history> Looking at the historical data, we can see that the spot price has been relatively stable, with some minor fluctuations. On January 4th, there was a dip in the spot price to 0.3946, but it quickly recovered. \
+    Overall, the spot price has remained within a narrow range over the past few days.\
+     The output should be:\
+     <prediction>: The most price will show a decreasing trend in the next 10 days, fluctuating between 0.3906 and 0.3683.  \
+     This trend indicates potential cost savings for cloud infrastructure users in the market.\
+     <confidence>: 0.95\
+    '
+    #prediction_agent = PredictionAgent(openai_api_key=your_api_key, openai_organization=your_org)
+    #prediction_agent.run(summary=summary, start=start, end=end, prediction_period=latest_period, token_limit=token_limit, latest_period=latest_period, examples)
 
--- tasks/dynamic vm pricing/scripts/generate_price_data.py
+++ tasks/dynamic vm pricing/scripts/generate_price_data.py
@@ -2,11 +2,14 @@ import utils
 import json
 import pandas as pd
 
-#rawdata = utils.read_aws_prices("aws/cost/us-east-1", summary=False)
-rawdata = utils.read_azure_prices("azure/cost")
+rawdata = utils.read_aws_prices("aws/cost/us-east-1", summary=False)
+#rawdata = utils.read_azure_prices("azure/cost")
 for key in rawdata.keys():
     df = pd.DataFrame(rawdata[key])
-    for zone in df[0].unique():
-        price = df[df[0]==zone][[1, 2]]
-        price = price.rename(columns={1: "Timestamp", 2: "SpotPrice"})
+    #for zone in df[0].unique():
+    #    price = df[df[0]==zone][[1, 2]]
+    #    price = price.rename(columns={1: "Timestamp", 2: "SpotPrice"})
+    for zone in df['AvailabilityZone'].unique():
+        price = df[df['AvailabilityZone']==zone][['Timestamp', 'SpotPrice']]
+        price = price.sort_values(by=['Timestamp'])
         price.to_csv(key+'_'+zone+'.txt', index=False)
